3b3fd0fbdef2f16a049f69ecb440f262f8449757
AG <38539655+gavrun@users.noreply.github.com>
2025-02-28 19:58:57 +0000

    added task 7 item 2 item 1 skipped

---
+++ Task07/WpfHello/CustomCommands.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfHello
+{
+    class CustomCommands
+    {
+        public static RoutedUICommand Launch { get; }
+
+        static CustomCommands()
+        {
+            InputGestureCollection myInputGestures = new InputGestureCollection();
+            myInputGestures.Add(new KeyGesture(Key.L, ModifierKeys.Control));
+            Launch = new RoutedUICommand("Launch", "Launch", typeof(CustomCommands), myInputGestures);
+        }
+
+    }
+}
